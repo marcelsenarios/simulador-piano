ead41add007e9f9987cbb0c30c17de7113b68492
Marcel <marcelsenarios@gmail.com>
2024-01-18 16:22:57 -0300

    trabalhando o toogle

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,5 +1,6 @@
 const pianoKeys = document.querySelectorAll('.piano-keys .key');
 const volumeSlider = document.querySelector('.volume-slider input');
+const keysCheck = document.querySelector('.keys-check input');
 let mapedKeys = [];
 let audio = new Audio("./src/tunes/a.wav");
 
@@ -14,7 +15,7 @@ const playTune = (key) => {
     }, 150)
 };
 
-pianoKeys.forEach((key)=> {
+pianoKeys.forEach((key) => {
     key.addEventListener("click", () => {
         playTune(key.dataset.key);
         mapedKeys.push(key.dataset.key);
@@ -22,7 +23,7 @@ pianoKeys.forEach((key)=> {
 });
 
 document.addEventListener('keydown', (e) => {
-    if(mapedKeys.includes(e.key)) {
+    if (mapedKeys.includes(e.key)) {
         playTune(e.key);
     }
 })
@@ -30,5 +31,9 @@ document.addEventListener('keydown', (e) => {
 const handleVolume = (e) => {
     audio.volume = e.target.value;
 }
+volumeSlider.addEventListener('input', handleVolume)
 
-volumeSlider.addEventListener('input', handleVolume)    
+const showHideKeys = () => {
+    pianoKeys.forEach(key => key.classList.toggle("hide"));
+}
+keysCheck.addEventListener("click", showHideKeys);
